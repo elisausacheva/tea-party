2c61d85b6b7f13e9852bf2f5bf7cc372c4fbf842
DAN-Andrey <andrey_nirvana@mail.ru>
2025-12-18 19:00:28 +0300

    добавлена модал идит ти форм

---
+++ client/src/entities/tea/api/ui/ModalEditTeaForm/ModalEditTeaForm.jsx
@@ -0,0 +1,54 @@
+import { CirclePlus, CircleX } from "lucide-react";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/esm/ButtonGroup";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+export default function ModalEditoneTeaForm({
+  show,
+  setShow,
+  tea,
+  updateHandler,
+}) {
+  return (
+    <>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header>
+          <Modal.Title>Изменение подарка</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={updateHandler}>
+            <Form.Group className="mb-3">
+              <Form.Label>Название</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Название"
+                defaultValue={tea.title}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Описание</Form.Label>
+              <Form.Control
+                type="text"
+                name="desc"
+                placeholder="Desc"
+                defaultValue={tea.desc}
+              />
+            </Form.Group>
+            <ButtonGroup>
+              <Button onClick={() => setShow(false)}>
+                Отменить <CircleX />
+              </Button>
+              <Button type="submit">
+                Сохранить <CirclePlus />
+              </Button>
+            </ButtonGroup>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
--- client/src/pages/OneTeaPage/OneTeaPage.jsx
+++ client/src/pages/OneTeaPage/OneTeaPage.jsx
@@ -7,9 +7,38 @@ import { TeaApi } from "../../entities/tea/TaeApi";
 export default function OneTeaPage() {
   const navigate = useNavigate();
   const [tea, setTea] = useState({});
+  const [show, setShow] = useState(false);
   // console.log('__________----', tea);
+  const params = useParams();
+  const { id } = params;
+  const { teaID } = params;
 
-  const { id } = useParams();
+const updateHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const targetData = event.target;
+      const dataForApi = Object.fromEntries(new FormData(targetData));
+      if (!dataForApi.name || !dataForApi.desc)
+        return alert("Заполните все поля");
+      const response = await TeaApi.update(teaID, dataForApi);
+      if (response.statusCode === 200) {
+        setTea(response.data);
+        setShow(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const deleteHandler = async () => {
+    try {
+      const response = await TeaApi.delete(teaID);
+      if (response.status === 204) navigate("/teas");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     const getOneTea = async () => {
@@ -25,10 +54,19 @@ export default function OneTeaPage() {
 
   return (
     <>
+     <ModalEditTeaForm
+        show={show}
+        setShow={setShow}
+        oneGift={tea}
+        updateHandler={updateHandler}
+      />
+      <Button onClick={deleteHandler}>
+        <Trash /> Удалить
+      </Button>
       <button onClick={() => navigate(-1)}>Назад</button>
       <h2>OneTeaPage</h2>
       <div> Фото: {tea.img}</div>
-      <div> Название: {tea.title}</div>
+      <div> Название: {tea.name}</div>
       <div> Текст: {tea.desc}</div>
     </>
   );
